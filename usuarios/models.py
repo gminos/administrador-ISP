50534ebbc2de3f344d206c6dca8fdfa5eb442816
gMinos <gjobelarmino@gmail.com>
2025-04-18 15:22:25 -0500

    Creacion tabla usuarios
    Con la tabla usuarios se llevara registro de todos los nombres,
    apellidos, documentos y telefonos. se tendra informacion repescto a cada
    usuario y cada sera obligatorio

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# tabla usuarios. por defecto defecto los campos son not null (no hay que expecificar)
+class Usuarios(models.Model):
+    documento = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} {self.apellido}"
