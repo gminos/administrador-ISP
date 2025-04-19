0bf81a0d4a6d4e660d31ebdea1a1cd34265a2682
gMinos <gjobelarmino@gmail.com>
2025-04-19 04:02:09 -0500

    Modelo Instalacion
    Creada segun el modelo de base de datos. creada para el seguimiento de
    las instalaciones

--- instalaciones/models.py
+++ instalaciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Intalacion(models.Model):
+    instalacion_id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(
+        "usuarios.Usuario", on_delete=models.CASCADE, related_name="instalacion"
+    )
+    fecha_instalacion = models.DateTimeField()
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = "instacion"
+        verbose_name_plural = "instalaciones"
