6ff6f4cdbbc0c490b9c012f12b0f578a431e0593
gMinos <gjobelarmino@gmail.com>
2025-04-19 03:29:10 -0500

    Aplicacion planes disponible en el panel administrador
    Disponible planes en el administrador para la creacion de planes para
    usuarios

--- planes/admin.py
+++ planes/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Plan, UsuarioPlan
 
-# Register your models here.
+
+@admin.register(UsuarioPlan)
+class UsuarioPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "usuario",
+        "plan",
+        "fecha_inico",
+        "fecha_cancelacion",
+        "estado_servicio",
+    )
+    search_fields = (
+        "estado_servicio",
+        "usuario__nombre",
+        "usuario__documento",
+    )
+    autocomplete_fields = ["usuario"]
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "cantidad_megas", "costo")
