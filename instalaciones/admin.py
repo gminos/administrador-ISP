80ce1d1bf69f264b9bb14580ab2ed8c13cb2fc74
gMinos <gjobelarmino@gmail.com>
2025-04-26 12:02:58 -0500

    Se agregan columnas para: documento, nombre, apellido
    Ahora en seccion de instalaciones estan las columnas para documento,
    nombre, apellido para tener coherencia con las demas secciones

--- instalaciones/admin.py
+++ instalaciones/admin.py
@@ -4,7 +4,15 @@ from .models import Intalacion
 
 @admin.register(Intalacion)
 class InstalacionAdmin(admin.ModelAdmin):
-    list_display = ("usuario", "fecha_instalacion", "costo")
-    search_fields = ("usuario__documento",
-                     "usuario__nombre", "usuario__apellido")
+    list_display = ("documento", "nombre", "apellido", "fecha_instalacion", "costo")
+    search_fields = ("usuario__documento", "usuario__nombre", "usuario__apellido")
     autocomplete_fields = ["usuario"]
+
+    def documento(self, obj):
+        return obj.usuario.documento
+
+    def nombre(self, obj):
+        return obj.usuario.nombre
+
+    def apellido(self, obj):
+        return obj.usuario.apellido
