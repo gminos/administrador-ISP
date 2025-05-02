a1af81aceaf461e619eccc6e8995287cba207673
gMinos <gjobelarmino@gmail.com>
2025-05-02 15:50:10 -0500

    Se crea el modelo Pago
    - Llevara el control de los pagos realizados por los usuarios
    - Se cambiado el nombre relativo al cual hace referencia factura con
    detalles

--- facturacion/models.py
+++ facturacion/models.py
@@ -18,8 +18,22 @@ class DetalleFactura(models.Model):
     ]
     detalle_factura_id = models.AutoField(primary_key=True)
     factura = models.ForeignKey(
-        "Factura", on_delete=models.CASCADE, related_name="factura"
+        "Factura", on_delete=models.CASCADE, related_name="detalles"
     )
     tipo_detalle = models.CharField(
         max_length=20, choices=TIPO_DETALLE_CHOICES)
     monto = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Pago(models.Model):
+    METODO_CHOICES = [("transferencia", "Transferencia"),
+                      ("efectivo", "Efectivo")]
+    ESTADO_CHOICES = [("cancelado", "Cancelado"), ("pendiente", "Pendiente")]
+    pago_id = models.AutoField(primary_key=True)
+    factura = models.ForeignKey(
+        "Factura", on_delete=models.CASCADE, related_name="pagos"
+    )
+    monto_pagado = models.DecimalField(max_digits=10, decimal_places=2)
+    metodo = models.CharField(max_length=15, choices=METODO_CHOICES)
+    estado = models.CharField(max_length=10, choices=ESTADO_CHOICES)
+    fecha_pago = models.DateTimeField(auto_now_add=True)
