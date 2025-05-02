10e41b5ee43d2b7c4168ab83cf8dc328273fe2b5
gMinos <gjobelarmino@gmail.com>
2025-05-02 15:21:29 -0500

    Modelo factura
    Se crea la base del esquema para facturar

--- facturacion/models.py
+++ facturacion/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Factura(models.Model):
+    factura_id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(
+        "usuarios.Usuario", on_delete=models.CASCADE, related_name="usuario"
+    )
+    fecha_emision = models.DateTimeField(blank=True, null=True)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2)
