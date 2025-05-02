e5f2d119d57a06af0e364dae94a0524e08e57e74
gMinos <gjobelarmino@gmail.com>
2025-05-02 16:12:45 -0500

    Se añadieron al admin
    Ahora es posible ver en el admin la sesion de Facturas, Detalles factura
    y Pagos

--- facturacion/admin.py
+++ facturacion/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Factura, Pago, DetalleFactura
 
-# Register your models here.
+
+@admin.register(Factura)
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ("usuario",)
+
+
+@admin.register(Pago)
+class PagoAdmin(admin.ModelAdmin):
+    list_display = ("factura",)
+
+
+@admin.register(DetalleFactura)
+class DetallePagoAdmin(admin.ModelAdmin):
+    list_display = ("factura",)
