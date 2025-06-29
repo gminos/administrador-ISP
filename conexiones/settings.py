5557ac219f7a860405d4c112eb3fb58fa361154a
gminos <gjosebelarmino@gmail.com>
2025-06-29 11:48:34 -0500

    Se agrega el nuevo template a los ajustes
    de esta forma se podra tomar el html creado para modificar el admin site
    personalizado

--- conexiones/settings.py
+++ conexiones/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "planes",
     "instalaciones",
     "facturacion",
+    "base",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = "conexiones.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates',],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
