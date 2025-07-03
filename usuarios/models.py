52cc2191f1e2503e45fa4deaff0dd88b7cd43d1f
gminos <gjosebelarmino@gmail.com>
2025-07-03 08:04:35 -0500

    Se elimina el modelo direcciones
    Debido a que no era tan necesario tenerlo por separado, se implemento la
    direccion en el modelo de usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -7,27 +7,28 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     telefono = models.CharField(max_length=10)
+    vereda = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
 
     class Meta:
         verbose_name = "cliente"
-        verbose_name_plural = "Crear/Editar cliente"
+        verbose_name_plural = "Agrega o modifica clientes"
 
 
 # Tabla Direccion para llevar el control de todas las direciones posibles del usuario
-class Direccion(models.Model):
-    direcciones_id = models.AutoField(primary_key=True)
-    usuario = models.ForeignKey(
-        "Usuario", on_delete=models.CASCADE, related_name="direcciones"
-    )
-    vereda = models.CharField(max_length=20)
-    descripcion = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return f"Informacion {self.usuario.nombre} {self.usuario.apellido}"
-
-    class Meta:
-        verbose_name = "direccion a cliente"
-        verbose_name_plural = "crear/Editar direccion de cliente"
+# class Direccion(models.Model):
+#     direcciones_id = models.AutoField(primary_key=True)
+#     usuario = models.ForeignKey(
+#         "Usuario", on_delete=models.CASCADE, related_name="direcciones"
+#     )
+#     vereda = models.CharField(max_length=20)
+#     descripcion = models.TextField(blank=True, null=True)
+#
+#     def __str__(self):
+#         return f"Informacion {self.usuario.nombre} {self.usuario.apellido}"
+#
+#     class Meta:
+#         verbose_name = "direccion a cliente"
+#         verbose_name_plural = "crear/Editar direccion de cliente"
