e1592fb82c543b4f7d88906586f025e0fc075545
gminos <gjosebelarmino@gmail.com>
2025-07-03 08:06:18 -0500

    Se elimina todo lo relacionado con direccion
    debido a que direccion ya no sera un modelo, se tiene que ajustar la
    vista usuarios ya que tenia consigo el modelo direccion. cambiando al
    nuevo campo vereda

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from planes.models import UsuarioPlan
 from instalaciones.models import Intalacion
-from .models import Usuario, Direccion
+from .models import Usuario
 from base.admin import admin_site
 
 
@@ -17,55 +17,58 @@ class UsuarioPlanInline(admin.TabularInline):
 
 
 # Crear direcciones al momento de crear un usuario
-class DireccionInline(admin.TabularInline):
-    model = Direccion
-    extra = 0
+# class DireccionInline(admin.TabularInline):
+#     model = Direccion
+#     extra = 0
 
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
+    actions = None
     list_display = (
         "nombre",
         "apellido",
+        "vereda",
         "telefono",
     )
     inlines = [
-        DireccionInline,
         InstalacionInline,
         UsuarioPlanInline,
     ]
     search_fields = (
         "nombre",
         "apellido",
+        "vereda"
     )
 
 
-@admin.register(Direccion)
-class DireccionAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-        "apellido",
-        "vereda",
-        "descripcion",
-    )
-    search_fields = (
-        "vereda",
-        "usuario__nombre",
-        "usuario__apellido",
-    )
-    autocomplete_fields = ["usuario"]
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if obj:
-            form.base_fields["usuario"].widget = forms.HiddenInput()
-        return form
-
-    def nombre(self, obj):
-        return obj.usuario.nombre
-
-    def apellido(self, obj):
-        return obj.usuario.apellido
+# @admin.register(Direccion)
+# class DireccionAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "nombre",
+#         "apellido",
+#         "vereda",
+#         "descripcion",
+#     )
+#     search_fields = (
+#         "vereda",
+#         "usuario__nombre",
+#         "usuario__apellido",
+#     )
+#     autocomplete_fields = ["usuario"]
+#
+#     def get_form(self, request, obj=None, **kwargs):
+#         form = super().get_form(request, obj, **kwargs)
+#         if obj:
+#             form.base_fields["usuario"].widget = forms.HiddenInput()
+#         return form
+#
+#     def nombre(self, obj):
+#         return obj.usuario.nombre
+#
+#     def apellido(self, obj):
+#         return obj.usuario.apellido
+#
 
 admin_site.register(Usuario, UsuarioAdmin)
-admin_site.register(Direccion, DireccionAdmin)
+# admin_site.register(Direccion, DireccionAdmin)
