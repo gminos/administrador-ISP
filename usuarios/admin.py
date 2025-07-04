c5a17fd66ba4649ff87199ffa60aa910b87e30e5
gminos <gjosebelarmino@gmail.com>
2025-07-04 08:39:57 -0500

    Se eliminan comentarios innecesarios
    se encontraban comentandas lineas relacionadas con el modelo direccion
    que actualmente no existe, asi que fueron eliminadas

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib import admin
 from planes.models import UsuarioPlan
 from instalaciones.models import Intalacion
@@ -14,12 +13,6 @@ class InstalacionInline(admin.TabularInline):
 class UsuarioPlanInline(admin.TabularInline):
     model = UsuarioPlan
     extra = 0
-
-
-# Crear direcciones al momento de crear un usuario
-# class DireccionInline(admin.TabularInline):
-#     model = Direccion
-#     extra = 0
 
 
 @admin.register(Usuario)
@@ -42,33 +35,4 @@ class UsuarioAdmin(admin.ModelAdmin):
     )
 
 
-# @admin.register(Direccion)
-# class DireccionAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "nombre",
-#         "apellido",
-#         "vereda",
-#         "descripcion",
-#     )
-#     search_fields = (
-#         "vereda",
-#         "usuario__nombre",
-#         "usuario__apellido",
-#     )
-#     autocomplete_fields = ["usuario"]
-#
-#     def get_form(self, request, obj=None, **kwargs):
-#         form = super().get_form(request, obj, **kwargs)
-#         if obj:
-#             form.base_fields["usuario"].widget = forms.HiddenInput()
-#         return form
-#
-#     def nombre(self, obj):
-#         return obj.usuario.nombre
-#
-#     def apellido(self, obj):
-#         return obj.usuario.apellido
-#
-
 admin_site.register(Usuario, UsuarioAdmin)
-# admin_site.register(Direccion, DireccionAdmin)
