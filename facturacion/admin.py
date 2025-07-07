58630b5107367a85edc374eaaa16c20e15f5afc7
gminos <gjosebelarmino@gmail.com>
2025-07-07 07:10:24 -0500

    Cambios en el admin en facturacion

--- facturacion/admin.py
+++ facturacion/admin.py
@@ -5,17 +5,32 @@ from base.admin import admin_site
 
 @admin.register(Factura)
 class FacturaAdmin(admin.ModelAdmin):
-    list_display = ("usuario",)
+    actions = None
+    list_display = ("cliente", "codigo", "periodo_inicio",
+                    "periodo_final", "monto_a_pagar")
+    search_fields = ("usuario__nombre", "usuario__apellido", "codigo")
+    autocomplete_fields = ["usuario"]
+
+    def cliente(self, obj):
+        return obj.usuario
 
 
 @admin.register(Pago)
 class PagoAdmin(admin.ModelAdmin):
-    list_display = ("factura",)
+    actions = None
+    list_display = (
+        "factura",
+        "estado",
+        "fecha_pago",
+        "metodo")
+    search_fields = ("usuario__nombre", "usuario__apellido")
 
 
 @admin.register(DetalleFactura)
 class DetallePagoAdmin(admin.ModelAdmin):
+    actions = None
     list_display = ("factura",)
+
 
 admin_site.register(Factura, FacturaAdmin)
 admin_site.register(Pago, PagoAdmin)
