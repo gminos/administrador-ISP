452476e805b39bc40fd348ad1bbd2fb17c782b61
gminos <gjosebelarmino@gmail.com>
2025-07-07 18:12:51 -0500

    pequeños cambios en el admin facturación
    - se comentan lineas de codigo donde se muestra la subsesion de pagos en
    el admin, ya que no es necesario que vea visualizada ni usada por
    separada
    - se agrega la subsesion pagos dentro de facturas, cuando se crea una
    factura puede ser añadido un pago o no
    - se crea un metodo para visualizar el estado del pago de la factura en
    la parte principal de facturación

--- facturacion/admin.py
+++ facturacion/admin.py
@@ -3,27 +3,39 @@ from .models import Factura, Pago, DetalleFactura
 from base.admin import admin_site
 
 
+class PagoInline(admin.TabularInline):
+    model = Pago
+    extra = 0
+
+
 @admin.register(Factura)
 class FacturaAdmin(admin.ModelAdmin):
-    actions = None
+    # actions = None
     list_display = ("cliente", "codigo", "periodo_inicio",
-                    "periodo_final", "monto_a_pagar")
-    search_fields = ("usuario__nombre", "usuario__apellido", "codigo")
+                    "periodo_final", "monto_a_pagar", "estado_pago")
+    search_fields = ("usuario__nombre", "usuario__apellido", "usuario__vereda",
+                     "codigo", "periodo_inicio")
     autocomplete_fields = ["usuario"]
+    inlines = [PagoInline]
 
     def cliente(self, obj):
         return obj.usuario
 
+    def estado_pago(self, obj):
+        pago = obj.pagos.first()
+        if pago:
+            return pago.estado.capitalize()
+        return "Pendiente"  # por defecto si no se ha creado el pago aún
+    estado_pago.short_description = "Estado del pago"
 
-@admin.register(Pago)
-class PagoAdmin(admin.ModelAdmin):
-    actions = None
-    list_display = (
-        "factura",
-        "estado",
-        "fecha_pago",
-        "metodo")
-    search_fields = ("usuario__nombre", "usuario__apellido")
+# @admin.register(Pago)
+# class PagoAdmin(admin.ModelAdmin):
+    # actions = None
+    # list_display = (
+    #     "factura"
+    #     "estado",
+    #     "fecha_pago",
+    #     "metodo")
 
 
 @admin.register(DetalleFactura)
@@ -33,5 +45,5 @@ class DetallePagoAdmin(admin.ModelAdmin):
 
 
 admin_site.register(Factura, FacturaAdmin)
-admin_site.register(Pago, PagoAdmin)
-admin_site.register(DetalleFactura, DetallePagoAdmin)
+# admin_site.register(Pago, PagoAdmin)
+# admin_site.register(DetalleFactura, DetallePagoAdmin)
