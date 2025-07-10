70063b8a5f18fc6d280ecc7edc2f3db29f4f0b91
gminos <gjosebelarmino@gmail.com>
2025-07-10 12:10:30 -0500

    Solucion a facturas pendientes y generacion del codigo de facturacion Fixes #8, Fixed #12
    - ya se lleva registro de las facturas que quedan pedientes de un mes y
    estas seran agregadas a la siguiente factura
    - se mejora la forma en que se obtiene el dia de corte para reconexion
    - ahora cuando las facturas se generan automaticamente tambien crea un
    pago de tipo mensualidad por el valor de la plan de ese momento, si se
    tiene una factura atrasada estos valores ya sera sumados a la factura
    nueva y aparecen tanto en el valor a pagar de la factura como en pago
    que se que agregar cuando se genera la factura

--- facturacion/management/commands/generar_facturas.py
+++ facturacion/management/commands/generar_facturas.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
-from facturacion.models import Factura
+from facturacion.models import Factura, Pago
 from usuarios.models import Usuario
 from datetime import date
 import calendar
+
+YEAR_INICIO = 2025
+MES_INICIO = 7
 
 
 class Command(BaseCommand):
@@ -10,11 +13,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         hoy = date.today()
+
         periodo_inicio = date(hoy.year, hoy.month, 1)
         ultimo_dia_del_mes = calendar.monthrange(hoy.year, hoy.month)[1]
         dia_final = 30 if ultimo_dia_del_mes >= 30 else ultimo_dia_del_mes
         periodo_final = date(hoy.year, hoy.month, dia_final)
-        reconexion = date(hoy.year, hoy.month + 1, 5)
+
+        mes_reconexion = hoy.month + 1 if hoy.month < 12 else 1
+        año_reconexion = hoy.year if hoy.month < 12 else hoy.year + 1
+        fecha_reconexion = date(año_reconexion, mes_reconexion, 5)
+
+        numero_periodo = (hoy.year - YEAR_INICIO) * 12 + \
+            (hoy.month - MES_INICIO) + 1
+        codigo_generado = f"ART-{numero_periodo:03d}"
 
         for usuario in Usuario.objects.all():
             if Factura.objects.filter(usuario=usuario, periodo_inicio=periodo_inicio).exists():
@@ -24,18 +35,37 @@ class Command(BaseCommand):
 
             instalaciones_activas = usuario.instalacion.filter(
                 servicio_activo=True)
-            monto = sum(
+            monto_actual = sum(
                 inst.plan.costo for inst in instalaciones_activas
                 if inst.plan and inst.plan.costo
             )
 
+            facturas_pendientes = Factura.objects.filter(
+                usuario=usuario
+            ).exclude(periodo_inicio=periodo_inicio)
+
+            deuda_anterior = 0
+            for factura in facturas_pendientes:
+                pagos_pendientes = factura.pagos.filter(
+                    estado="se acumula")
+                deuda_anterior += sum(pago.monto_pagado for pago in pagos_pendientes)
+
+            monto_total = monto_actual + deuda_anterior
+
             factura = Factura.objects.create(
                 usuario=usuario,
-                codigo=hoy.month,
+                codigo_factura=codigo_generado,
                 periodo_inicio=periodo_inicio,
                 periodo_final=periodo_final,
-                fecha_reconexion=reconexion,
-                monto_a_pagar=monto
+                fecha_reconexion=fecha_reconexion,
+                monto_a_pagar=monto_total,
+            )
+
+            Pago.objects.create(
+                factura=factura,
+                monto_pagado=monto_total,
+                metodo_pago="no aplica",
+                estado="pendiente"
             )
 
             self.stdout.write(self.style.SUCCESS(
