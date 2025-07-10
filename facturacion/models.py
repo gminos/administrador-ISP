bed80176deeb306fc6aed5479ab6ac7bec4971b8
gminos <gjosebelarmino@gmail.com>
2025-07-10 02:24:51 -0500

    Se ajusta el modelo en facturacion
    - el modelo de Factura se le ha agregado el campo fecha_reconexion, es
    relevante para el modelo de datos
    - el modelo Pago ha sido actualizado con el campo tipo_pago para
    determinar si el pago registrado es de tipo mensualidad o reconexion
    - se cambia de lugar las constantes METODO_CHOICES y ESTADO_CHOICES
    - es comentado el modelo DetalleFactura, de momento no esta siendo
    utilizado

--- facturacion/models.py
+++ facturacion/models.py
@@ -5,6 +5,12 @@ MESES = [
     "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
 ]
 
+METODO_CHOICES = [("transferencia", "Transferencia"),
+                  ("efectivo", "Efectivo"), ("no aplica", "No aplica")]
+ESTADO_CHOICES = [("pagado", "Pagado"), ("pendiente", "Pendiente")]
+TIPO_PAGOS_CHOICES = [("mensualidad", "Mensualidad"),
+                      ("reconexion", "Reconexion")]
+
 
 class Factura(models.Model):
     factura_id = models.AutoField(primary_key=True)
@@ -14,6 +20,7 @@ class Factura(models.Model):
     codigo = models.IntegerField(null=True)
     periodo_inicio = models.DateField(null=True)
     periodo_final = models.DateField(null=True)
+    fecha_reconexion = models.DateField(null=True)
     monto_a_pagar = models.DecimalField(
         max_digits=10, decimal_places=2, null=True)
 
@@ -27,31 +34,31 @@ class Factura(models.Model):
         verbose_name_plural = "Administra facturas y pagos"
 
 
-class DetalleFactura(models.Model):
-    TIPO_DETALLE_CHOICES = [
-        ("mensualidad", "Mensualidad"),
-        ("instalación", "Instalación"),
-        ("reconexión", "Reconexión"),
-    ]
-    detalle_factura_id = models.AutoField(primary_key=True)
-    factura = models.ForeignKey(
-        "Factura", on_delete=models.CASCADE, related_name="detalles"
-    )
-    tipo_detalle = models.CharField(
-        max_length=20, choices=TIPO_DETALLE_CHOICES)
-    monto = models.DecimalField(max_digits=10, decimal_places=2)
+# class DetalleFactura(models.Model):
+#     TIPO_DETALLE_CHOICES = [
+#         ("mensualidad", "Mensualidad"),
+#         ("instalación", "Instalación"),
+#         ("reconexión", "Reconexión"),
+#     ]
+#     detalle_factura_id = models.AutoField(primary_key=True)
+#     factura = models.ForeignKey(
+#         "Factura", on_delete=models.CASCADE, related_name="detalles"
+#     )
+#     tipo_detalle = models.CharField(
+#         max_length=20, choices=TIPO_DETALLE_CHOICES)
+#     monto = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class Pago(models.Model):
-    METODO_CHOICES = [("transferencia", "Transferencia"),
-                      ("efectivo", "Efectivo")]
-    ESTADO_CHOICES = [("pagado", "Pagado"), ("pendiente", "Pendiente")]
     pago_id = models.AutoField(primary_key=True)
     factura = models.ForeignKey(
         "Factura", on_delete=models.CASCADE, related_name="pagos", null=True
     )
     monto_pagado = models.DecimalField(max_digits=10, decimal_places=2)
-    metodo = models.CharField(max_length=15, choices=METODO_CHOICES)
+    tipo_pago = models.CharField(
+        max_length=15, choices=TIPO_PAGOS_CHOICES, default="mensualidad", null=True)
+    metodo_pago = models.CharField(
+        max_length=15, choices=METODO_CHOICES, default="no aplica")
     estado = models.CharField(
         max_length=10, choices=ESTADO_CHOICES, default="pendiente"
     )
