c2449893bdb49cf0419831b72975977ec1ad3407
gminos <gjosebelarmino@gmail.com>
2026-02-17 09:06:51 -0500

    chore: Ajustar configuracion de entorno de desarrollo y variables de entorno

--- conexiones/settings.py
+++ conexiones/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DJANGO_DEBUG", "false").lower() == True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
