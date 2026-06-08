f62edf2d50bf3225f10ea3529c0ced35880e9915
gminos <gjosebelarmino@gmail.com>
2026-06-08 17:46:25 -0500

    refactor: Optimizar generacion automatica de facturas y soportar multiples ciclos de facturacion

--- facturacion/management/commands/generar_facturas.py
+++ facturacion/management/commands/generar_facturas.py
@@ -1,6 +1,7 @@
+from django.db import transaction
 from django.core.management.base import BaseCommand
-from facturacion.models import Factura, Pago
-from clientes.models import Cliente
+from facturacion.models import Factura
+from instalaciones.models import Instalacion
 from datetime import date
 import calendar
 
@@ -8,51 +9,59 @@ import calendar
 class Command(BaseCommand):
     help = 'Genera pagos del mes actual para todos los usuarios cuyo servicio se encuentre activo'
 
+    def calculo_fechas(self, ciclo_facturacion: int) -> tuple[date, date, date]:
+        fecha_hoy = date.today()
+        year_siguiente = fecha_hoy.year if fecha_hoy.month < 12 else fecha_hoy.year + 1
+        mes_siguiente = fecha_hoy.month + 1 if fecha_hoy.month < 12 else 1
+
+        if ciclo_facturacion == 30:
+            periodo_inicio = date(fecha_hoy.year, fecha_hoy.month, 1)
+            ultimo_dia_del_mes = calendar.monthrange(fecha_hoy.year, fecha_hoy.month)[1]
+            dia_final = 30 if ultimo_dia_del_mes >= 30 else ultimo_dia_del_mes
+            periodo_final = date(fecha_hoy.year, fecha_hoy.month, dia_final)
+            fecha_reconexion = date(year_siguiente, mes_siguiente, 3)
+
+            return periodo_inicio, periodo_final, fecha_reconexion
+
+        periodo_inicio = date(fecha_hoy.year, fecha_hoy.month, 15)
+        periodo_final = date(year_siguiente, mes_siguiente, 14)
+        fecha_reconexion = date(year_siguiente, mes_siguiente, 17)
+
+        return periodo_inicio, periodo_final, fecha_reconexion
+
     def handle(self, *args, **kwargs):
-        hoy = date.today()
+        fecha_hoy = date.today()
+        ultimo_dia_del_mes = calendar.monthrange(fecha_hoy.year, fecha_hoy.month)[1]
+        ciclos_a_cobrar = [fecha_hoy.day]
 
-        periodo_inicio = date(hoy.year, hoy.month, 1)
-        ultimo_dia_del_mes = calendar.monthrange(hoy.year, hoy.month)[1]
-        dia_final = 30 if ultimo_dia_del_mes >= 30 else ultimo_dia_del_mes
-        periodo_final = date(hoy.year, hoy.month, dia_final)
+        if fecha_hoy.day == ultimo_dia_del_mes and ultimo_dia_del_mes < 30:
+            ciclos_a_cobrar.append(30)
 
-        mes_reconexion = hoy.month + 1 if hoy.month < 12 else 1
-        year_reconexion = hoy.year if hoy.month < 12 else hoy.year + 1
-        fecha_reconexion = date(year_reconexion, mes_reconexion, 5)
 
-        for cliente in Cliente.objects.all():
-            if Factura.objects.filter(cliente=cliente, periodo_inicio=periodo_inicio).exists():
+        instalaciones_del_dia = Instalacion.objects.filter(ciclo_facturacion__in=ciclos_a_cobrar, servicio_activo=True)
+        facturas_nuevas = []
+
+        for instalacion in instalaciones_del_dia:
+            periodo_inicio, periodo_final, fecha_reconexion = self.calculo_fechas(instalacion.ciclo_facturacion)
+
+            if Factura.objects.filter(instalacion=instalacion, periodo_inicio=periodo_inicio).exists():
                 self.stdout.write(self.style.WARNING(
-                    f"Ya existe factura para {cliente}"))
+                    f"Ya existe factura para {instalacion.cliente}"))
                 continue
 
-            instalaciones_activas = cliente.instalaciones.filter(
-                servicio_activo=True)
-
-            if not instalaciones_activas.exists():
-                self.stdout.write(self.style.WARNING(
-                    f"No hay servicios activos para {cliente}"))
-                continue
-
-            monto_a_pagar = sum(
-                inst.plan.costo for inst in instalaciones_activas
-                if inst.plan and inst.plan.costo
-            )
-
-            factura = Factura.objects.create(
-                cliente=cliente,
+            factura_nueva = Factura(
+                instalacion=instalacion,
                 periodo_inicio=periodo_inicio,
                 periodo_final=periodo_final,
                 fecha_reconexion=fecha_reconexion,
+                monto_total = instalacion.plan.costo
             )
 
-            Pago.objects.create(
-                factura=factura,
-                monto_pagado=monto_a_pagar,
-                metodo_pago="no aplica",
-                estado="pendiente"
-            )
+            facturas_nuevas.append(factura_nueva)
 
             self.stdout.write(self.style.SUCCESS(
-                f"Pago generado para {cliente}"
+                f"Factura genera para: {instalacion.cliente.nombre}"
             ))
+
+        with transaction.atomic():
+            Factura.objects.bulk_create(facturas_nuevas)
